69cd69c07ec334efd2f77524378bd261c08fd24b
nickpolarits <Nikhil@polarits.com>
2017-12-26 11:11:21 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const publicPath=path.join(__dirname,'../public');
+const  port = process.env.PORT || 3000;
+
+const express = require('express');
+
+const app = express();
+app.use(express.static(publicPath));
+app.listen(port,()=>{
+	console.log(`Port started on ${port}`);
+})
