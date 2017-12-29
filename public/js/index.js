a2e6117f97452e0b90b502c26c4c4c54388d5595
nickpolarits <Nikhil@polarits.com>
2017-12-29 11:22:01 +0530

    adding rooms

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,20 @@
 var socket = io();
+
+function scrollToBottom () {
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child')
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
 		socket.on('connect',function(){
 			console.log('connected to server');
 			// socket.emit('createEmail',{
@@ -22,6 +38,7 @@ var socket = io();
 			// var li = jQuery('<li></li>');
 			// li.text(`${message.from} ${formattedTime}:${message.text}`);
 			jQuery('#messages').append(html);
+			scrollToBottom();
 		});
 
 		socket.on('disconnect',function(){
@@ -70,6 +87,7 @@ var socket = io();
 			// a.attr('href',message.url);
 			// li.append(a);
 			jQuery('#messages').append(html);
+			scrollToBottom();
 		});
 
 		var myLocation = jQuery('#send-location');
